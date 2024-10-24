d15141ce87432d8af907fafe67771565a3498995
kalistaniacasey <162282241+kalistaniacasey@users.noreply.github.com>
2024-10-24 14:54:24 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Buku novel = new Buku("Nuna baik", "Annisa salma", 2023, "Pustaka Nusantara", "Motivasi Harian");
+
+        novel.tampilkanInfo();
+    }
+}
